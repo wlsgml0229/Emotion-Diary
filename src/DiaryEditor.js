95324671446c3c3552172be577ed91ab5d1855b9
jinhee kim <wlsgml0229@naver.com>
2022-10-24 23:57:11 +0900

    기본적인 스타일링 추가

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -4,6 +4,7 @@ const DiaryEditor = () => {
   const [state, setState] = useState({
     author: "",
     content: "",
+    emotion: 1,
   });
   // useState 사용하여 상태변수 생성
   // 각각쓰는 방법말고 위와같이 정리하여 쓸수 있다.
@@ -17,6 +18,10 @@ const DiaryEditor = () => {
       ...state,
       [e.target.name]: e.target.value,
     });
+  };
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장 성공");
   };
   return (
     <div className="DiaryEditor">
@@ -36,13 +41,20 @@ const DiaryEditor = () => {
         />
       </div>
       <div>
-        <select>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
           <option value={1}>1</option>
           <option value={2}>2</option>
           <option value={3}>3</option>
           <option value={4}>4</option>
           <option value={5}>5</option>
         </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
