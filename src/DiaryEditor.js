8294d13a9d781a9e98876294037eaa70173327a6
jinhee kim <wlsgml0229@naver.com>
2022-10-24 23:48:52 +0900

    사용자 입력 처리 - 상태값 변화 시키기

---
+++ src/DiaryEditor.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
+  // useState 사용하여 상태변수 생성
+  // 각각쓰는 방법말고 위와같이 정리하여 쓸수 있다.
+  //   const [author, setAuthor] = useState("");
+  //   const [content, setContent] = useState("");
+
+  // 이벤트핸들러도 합쳐서 사용하기
+  // input 에 지정한 name 값과 value값으로 값을 변경시킴
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <select>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
