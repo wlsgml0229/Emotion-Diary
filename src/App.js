21a93e1afe2716884793b5188ff136726c02d5f0
jinhee kim <wlsgml0229@naver.com>
2022-11-07 22:27:44 +0900

    일기 리스트 api 호출후 렌더링

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -7,6 +7,31 @@ function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  // getData 는 api 호출후 데이터 가공하고 setData를 통해서 화면이 렌더링 되었을때 일기 목록을 불러오는 것을 구현
+  const getData = async () => {
+    const result = await fetch(
+      `https://jsonplaceholder.typicode.com/comments`
+    ).then((res) => res.json());
+    console.log(result);
+
+    // 일기데이터에 맞추기위해서 데이터 변경 (랜덤데이터 0~4까지 + 1) 해준 것
+    const initData = result.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  //app 컴포넌트가 마운트 됐을때 getData호출
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
